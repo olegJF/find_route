f9936144a79e8743d933d15cfd0684307a719da3
OlegPC <jf2.oleg@gmail.com>
2020-11-22 16:55:29 +0200

    Создание модели и руссификация приложение Города

--- cities/apps.py
+++ cities/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class CitiesConfig(AppConfig):
     name = 'cities'
+    verbose_name = 'Населенные пункты'
--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Город')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['name']
