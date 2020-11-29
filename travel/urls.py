ca83805f8ed11ab925e3c555a5a2cf02fc23c977
OlegPC <jf2.oleg@gmail.com>
2020-11-29 10:55:24 +0200

    Добавление пунктов меню в раздел навигации

--- travel/urls.py
+++ travel/urls.py
@@ -21,6 +21,6 @@ from travel.views import home, about
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
-    path('home/', home),
+    path('', home, name='home'),
     path('about/', about),
 ]
