aa2e54b0ebf4e6354c816528e09899ae0f5db96d
OlegPC <jf2.oleg@gmail.com>
2020-11-29 15:53:47 +0200

    get_absolute_url

--- cities/models.py
+++ cities/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class City(models.Model):
@@ -11,3 +12,6 @@ class City(models.Model):
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
         ordering = ['name']
+
+    def get_absolute_url(self):
+        return reverse('cities:detail', kwargs={'pk': self.pk})
