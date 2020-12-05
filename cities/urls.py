646c75b5bc354edf8e6d717c8adbc65f047dcabb
OlegPC <jf2.oleg@gmail.com>
2020-12-05 14:14:38 +0200

    CityUpdateView

--- cities/urls.py
+++ cities/urls.py
@@ -5,6 +5,7 @@ from cities.views import *
 urlpatterns = [
     path('', home, name='home'),
     path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
     path('add/', CityCreateView.as_view(), name='create'),
 
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, UpdateView
 
 from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 __all__ = (
-    'home', 'CityDetailView', 'CityCreateView'
+    'home', 'CityDetailView', 'CityCreateView', 'CityUpdateView'
 )
 
 
@@ -38,4 +38,11 @@ class CityCreateView(CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
     success_url = reverse_lazy('cities:home')
