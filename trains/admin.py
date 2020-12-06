aedef69bb32bb8239687955a27dcc61b0335c367
OlegPC <jf2.oleg@gmail.com>
2020-12-06 15:07:29 +0200

    Улучшение отображений списка поездов в админке.

--- trains/admin.py
+++ trains/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from trains.models import Train
 
-admin.site.register(Train)
+
+class TrainAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Train
+    list_display = ('name', 'from_city', 'to_city', 'travel_time')
+    list_editable = ('travel_time', )  # 'from_city'
+
+
+admin.site.register(Train, TrainAdmin)
