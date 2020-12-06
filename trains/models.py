667be9f9f8a62963f65ac99251836372a897732b
OlegPC <jf2.oleg@gmail.com>
2020-12-06 14:49:03 +0200

    Метод clean()

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from cities.models import City
@@ -24,6 +25,20 @@ class Train(models.Model):
         verbose_name = 'Поезд'
         verbose_name_plural = 'Поезда'
         ordering = ['travel_time']
+
+    def clean(self):
+        if self.from_city == self.to_city:
+            raise ValidationError('Изменить город прибытия')
+        qs = Train.objects.filter(
+            from_city=self.from_city, to_city=self.to_city,
+            travel_time=self.travel_time).exclude(pk=self.pk)
+        # Train == self.__class__
+        if qs.exists():
+            raise ValidationError('Измените время в пути')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
 
 class TrainTest(models.Model):
