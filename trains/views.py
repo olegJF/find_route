73199921b2ecc370e9c8b8f9d45496978122a0fa
OlegPC <jf2.oleg@gmail.com>
2020-12-06 15:43:07 +0200

    TrainListView

--- trains/views.py
+++ trains/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, \
+    ListView
 
-# Create your views here.
+# from trains.forms import HtmlForm, TrainForm
+from trains.models import Train
+
+__all__ = (
+    'home', 'TrainListView',
+    # 'TrainDetailView', 'TrainCreateView', 'TrainUpdateView',
+    # 'TrainDeleteView',
+)
+
+
+def home(request, pk=None):
+    qs = Train.objects.all()
+    lst = Paginator(qs, 5)
+    page_number = request.GET.get('page')
+    page_obj = lst.get_page(page_number)
+    context = {'page_obj': page_obj,}
+    return render(request, 'trains/home.html', context)
+
+
+class TrainListView(ListView):
+    paginate_by = 5
+    model = Train
+    template_name = 'trains/home.html'
+
+#
+# class TrainDetailView(DetailView):
+#     queryset = Train.objects.all()
+#     template_name = 'trains/detail.html'
+#
+#
+# class TrainCreateView(SuccessMessageMixin, CreateView):
+#     model = Train
+#     form_class = TrainForm
+#     template_name = 'trains/create.html'
+#     success_url = reverse_lazy('trains:home')
+#     success_message = "Город успешно создан"
+#
+#
+# class TrainUpdateView(SuccessMessageMixin, UpdateView):
+#     model = Train
+#     form_class = TrainForm
+#     template_name = 'trains/update.html'
+#     success_url = reverse_lazy('trains:home')
+#     success_message = "Город успешно отредактирован"
+#
+#
+# class TrainDeleteView(DeleteView):
+#     model = Train
+#     # template_name = 'trains/delete.html'
+#     success_url = reverse_lazy('trains:home')
+#
+#     def get(self, request, *args, **kwargs):
+#         messages.success(request, 'Город успешно удален')
+#         return self.post(request, *args, **kwargs)
+
+
+
--- travel/urls.py
+++ travel/urls.py
@@ -21,6 +21,7 @@ from travel.views import home, about
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
+    path('trains/', include(('trains.urls', 'trains'))),
     path('', home, name='home'),
     path('about/', about),
 ]
