390a29f9d30e3ed9cc8ad8d6ceab475fe625e105
OlegPC <jf2.oleg@gmail.com>
2020-12-12 16:05:02 +0200

    добавил функцию get_graph

--- routes/utils.py
+++ routes/utils.py
@@ -1,4 +1,15 @@
+from trains.models import Train
+
+
+def get_graph(qs):
+    graph = {}
+    for q in qs:
+        graph.setdefault(q.from_city_id, set())
+        graph[q.from_city_id].add(q.to_city_id)
+    return graph
 
 
 def get_routes(request, form) -> dict:
-    pass
+    qs = Train.objects.all()
+    graph = get_graph(qs)
+
