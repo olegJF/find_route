a6aa47f9777cac1c756927dff1ea6a7107db22b0
OlegPC <jf2.oleg@gmail.com>
2020-12-12 12:31:31 +0200

    select2

--- routes/forms.py
+++ routes/forms.py
@@ -5,18 +5,18 @@ from cities.models import City
 class RouteForm(forms.Form):
     from_city = forms.ModelChoiceField(
         label='Откуда', queryset=City.objects.all(), widget=forms.Select(
-            attrs={'class': 'form-control'}
+            attrs={'class': 'form-control js-example-basic-single'}
         )
     )
     to_city = forms.ModelChoiceField(
         label='Куда', queryset=City.objects.all(), widget=forms.Select(
-            attrs={'class': 'form-control'}
+            attrs={'class': 'form-control js-example-basic-single'}
         )
     )
     cities = forms.ModelMultipleChoiceField(
         label='Через города', queryset=City.objects.all(),
         required=False, widget=forms.SelectMultiple(
-            attrs={'class': 'form-control'}
+            attrs={'class': 'form-control js-example-basic-multiple'}
         )
     )
     travelling_time = forms.IntegerField(
