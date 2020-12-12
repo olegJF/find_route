abe722d994a11580a919bae6c0740c57dc3d5325
OlegPC <jf2.oleg@gmail.com>
2020-12-12 13:23:09 +0200

    Отправка данных в функцию find_routes

--- routes/views.py
+++ routes/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from routes.forms import RouteForm
 
@@ -5,3 +6,14 @@ from routes.forms import RouteForm
 def home(request):
     form = RouteForm()
     return render(request, 'routes/home.html', {'form': form})
+
+
+def find_routes(request):
+    if request.method == "POST":
+        form = RouteForm(request.POST)
+        a = 1
+        return render(request, 'routes/home.html', {'form': form})
+    else:
+        form = RouteForm()
+        messages.error(request, "Нет данных для поиска")
+        return render(request, 'routes/home.html', {'form': form})
--- travel/urls.py
+++ travel/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from routes.views import home
+from routes.views import home, find_routes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
     path('', home, name='home'),
+    path('find_routes/', find_routes, name='find_routes'),
 ]
