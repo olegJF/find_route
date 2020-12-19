2d547bd5f81faa7bbf02d446264f7ecedd0c2e45
OlegPC <jf2.oleg@gmail.com>
2020-12-19 14:00:22 +0200

    Добавил add_route и форму со скрытыми полями

--- routes/views.py
+++ routes/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from routes.forms import RouteForm
 from routes.utils import get_routes
 
@@ -24,3 +24,13 @@ def find_routes(request):
         form = RouteForm()
         messages.error(request, "Нет данных для поиска")
         return render(request, 'routes/home.html', {'form': form})
+
+
+def add_route(request):
+    if request.method == "POST":
+        context = {}
+        data = request.POST
+        return render(request, 'routes/create.html', context)
+    else:
+        messages.error(request, "Невозможно сохранить несуществующий маршрут")
+        return redirect('/')
--- travel/urls.py
+++ travel/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from routes.views import home, find_routes
+from routes.views import home, find_routes, add_route
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('trains/', include(('trains.urls', 'trains'))),
     path('', home, name='home'),
     path('find_routes/', find_routes, name='find_routes'),
+    path('add_route/', add_route, name='add_route'),
 ]
