9166cb30bf22cebbb5526a5eee362687414ddf19
OlegPC <jf2.oleg@gmail.com>
2020-12-26 14:52:00 +0200

    возникновение ошибок при создании дублей

--- routes/tests.py
+++ routes/tests.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from cities.models import City
@@ -33,3 +34,27 @@ class AllTestsCase(TestCase):
                   travel_time=4)
         ]
         Train.objects.bulk_create(lst)
+
+    def test_model_city_duplicate(self):
+        '''Тестирование возникновения ошибки при создании дубля города'''
+        city = City(name="A")
+        with self.assertRaises(ValidationError):
+            city.full_clean()
+
+    def test_model_train_duplicate(self):
+        '''Тестирование возникновения ошибки при создании дубля поезда'''
+        train = Train(name='t1', from_city=self.city_A, to_city=self.city_B,
+                      travel_time=129)
+        with self.assertRaises(ValidationError):
+            train.full_clean()
+
+    def test_model_train_train_duplicate(self):
+        '''Тестирование возникновения ошибки при создании дубля поезда'''
+        train = Train(name='t1234', from_city=self.city_A, to_city=self.city_B,
+                      travel_time=9)
+        with self.assertRaises(ValidationError):
+            train.full_clean()
+        try:
+            train.full_clean()
+        except ValidationError as e:
+            self.assertEqual({'__all__': ['Измените время в пути']}, e.message_dict)
